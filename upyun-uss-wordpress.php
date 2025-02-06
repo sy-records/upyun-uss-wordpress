3baf99c5c3cafe2990977dd808969d8b42d504c4
sy-records <52o@qq52o.cn>
2025-02-06 18:16:21 +0800

    Add multi-site support and optimize code

--- upyun-uss-wordpress.php
+++ upyun-uss-wordpress.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: USS Upyun
 Plugin URI: https://github.com/sy-records/upyun-uss-wordpress
-Description: 使用又拍云云存储USS作为附件存储空间。（This is a plugin that uses UPYUN Storage Service for attachments remote saving.）
-Version: 1.4.0
+Description: 使用又拍云云存储USS作为附件存储空间。(This is a plugin that uses UPYUN Storage Service for attachments remote saving.)
+Version: 1.5.0
 Author: 沈唁
 Author URI: https://qq52o.me
 License: Apache 2.0
@@ -11,7 +11,7 @@ License: Apache 2.0
 
 require_once 'sdk/vendor/autoload.php';
 
-define('USS_VERSION', '1.4.0');
+define('USS_VERSION', '1.5.0');
 define('USS_BASEFOLDER', plugin_basename(dirname(__FILE__)));
 
 use Upyun\Upyun;
@@ -22,11 +22,9 @@ if (!function_exists('get_home_path')) {
 }
 
 // 初始化选项
-register_activation_hook(__FILE__, 'uss_set_options');
-// 初始化选项
-function uss_set_options()
+function uss_get_default_options()
 {
-    $options = [
+    return [
         'bucket' => '',
         'OperatorName' => '',
         'OperatorPwd' => '',
@@ -36,12 +34,16 @@ function uss_set_options()
         'update_file_name' => 'false', // 是否重命名文件名
         'image_process' => '', // 图片处理 https://console.upyun.com/services/process/
     ];
-    add_option('uss_options', $options, '', 'yes');
+}
+register_activation_hook(__FILE__, 'uss_set_options');
+function uss_set_options()
+{
+    add_option('uss_options', uss_get_default_options(), '', 'yes');
 }
 
 function uss_get_client()
 {
-    $uss_opt = get_option('uss_options', true);
+    $uss_opt = get_option('uss_options', uss_get_default_options());
     $bucket = esc_attr($uss_opt['bucket']);
     $OperatorName = esc_attr($uss_opt['OperatorName']);
     $OperatorPwd = esc_attr($uss_opt['OperatorPwd']);
@@ -52,9 +54,9 @@ function uss_get_client()
 /**
  * 上传
  *
- * @param $object
- * @param $file
- * @param false $no_local_file
+ * @param string $object
+ * @param string $file
+ * @param bool $no_local_file
  * @return false|void
  * @throws Exception
  */
@@ -73,7 +75,7 @@ function uss_file_upload($object, $file, $no_local_file = false)
     $client = uss_get_client();
     $client->write($object, $file_resource);
     if (is_resource($file_resource)) {
-        fclose($file);
+        fclose($file_resource);
     }
     if ($no_local_file) {
         uss_delete_local_file($file);
@@ -82,7 +84,7 @@ function uss_file_upload($object, $file, $no_local_file = false)
 
 function uss_sanitize_file_name($filename)
 {
-    $uss_options = get_option('uss_options');
+    $uss_options = get_option('uss_options', uss_get_default_options());
     switch ($uss_options['update_file_name']) {
         case 'md5':
             return  md5($filename) . '.' . pathinfo($filename, PATHINFO_EXTENSION);
@@ -102,7 +104,7 @@ add_filter('sanitize_file_name', 'uss_sanitize_file_name', 10, 1);
  */
 function uss_is_delete_local_file()
 {
-    $uss_options = get_option('uss_options', true);
+    $uss_options = get_option('uss_options', uss_get_default_options());
     return esc_attr($uss_options['nolocalsaving']) == 'true';
 }
 
@@ -164,7 +166,7 @@ function uss_delete_uss_files($files)
  */
 function uss_upload_attachments($metadata)
 {
-    $mime_types = get_allowed_mime_types();
+    $mime_types = wp_get_mime_types();
     $image_mime_types = [
         $mime_types['jpg|jpeg|jpe'],
         $mime_types['gif'],
@@ -173,7 +175,6 @@ function uss_upload_attachments($metadata)
         $mime_types['tiff|tif'],
         $mime_types['webp'],
         $mime_types['ico'],
-        $mime_types['heic'],
     ];
 
     // 例如mp4等格式 上传后根据配置选择是否删除 删除后媒体库会显示默认图片 点开内容是正常的
@@ -211,7 +212,7 @@ function uss_upload_thumbs($metadata)
     $basedir = $wp_uploads['basedir'];
     $upload_path = get_option('upload_path');
     //获取uss插件的配置信息
-    $uss_options = get_option('uss_options', true);
+    $uss_options = get_option('uss_options', uss_get_default_options());
     $no_local_file = esc_attr($uss_options['nolocalsaving']) == 'true';
     $no_thumb = esc_attr($uss_options['nothumb']) == 'true';
 
@@ -220,8 +221,8 @@ function uss_upload_thumbs($metadata)
     $file = $basedir . '/' . $metadata['file'];
     if ($upload_path != '.') {
         $path_array = explode($upload_path, $file);
-        if (isset($path_array[1]) && !empty($path_array[1])) {
-            $object = '/' . $upload_path . $path_array[1];
+        if (count($path_array) >= 2) {
+            $object = '/' . $upload_path . end($path_array);
         }
     } else {
         $object = '/' . $metadata['file'];
@@ -293,19 +294,20 @@ if (substr_count($_SERVER['REQUEST_URI'], '/update.php') <= 0) {
  */
 function uss_delete_remote_attachment($post_id)
 {
+    $wp_uploads = wp_upload_dir();
+    $basedir = $wp_uploads['basedir'];
+    $upload_path = str_replace(get_home_path(), '', $basedir);
+    // 获取图片类附件的meta信息
     $meta = wp_get_attachment_metadata($post_id);
-    $upload_path = get_option('upload_path');
-    if ($upload_path == '') {
-        $upload_path = 'wp-content/uploads';
-    }
 
     if (!empty($meta['file'])) {
         $deleteObjects = [];
+
         // meta['file']的格式为 "2020/01/wp-bg.png"
         $file_path = $upload_path . '/' . $meta['file'];
-        $deleteObjects[] = $file_path;
-
         $dirname = dirname($file_path) . '/';
+
+        $deleteObjects[] = $file_path;
 
         // 超大图原图
         if (!empty($meta['original_image'])) {
@@ -333,17 +335,19 @@ function uss_delete_remote_attachment($post_id)
         if ($link) {
             if ($upload_path != '.') {
                 $file_info = explode($upload_path, $link);
-                if (isset($file_info[1])) {
-                    uss_delete_uss_file($upload_path . $file_info[1]);
+                if (count($file_info) >= 2) {
+                    $file = $upload_path . end($file_info);
                 }
             } else {
-                $uss_options = get_option('uss_options', true);
+                $uss_options = get_option('uss_options', uss_get_default_options());
                 $uss_upload_url = esc_attr($uss_options['upload_url_path']);
                 $file_info = explode($uss_upload_url, $link);
-                if (isset($file_info[1])) {
-                    uss_delete_uss_file($file_info[1]);
+                if (count($file_info) >= 2) {
+                    $file = end($file_info);
                 }
             }
+
+            uss_delete_uss_file($file);
         }
     }
 }
@@ -405,7 +409,7 @@ add_filter('wp_calculate_image_srcset', 'uss_custom_image_srcset', 10, 5);
 
 function uss_custom_image_srcset($sources, $size_array, $image_src, $image_meta, $attachment_id)
 {
-    $option = get_option('uss_options');
+    $option = get_option('uss_options', uss_get_default_options());
     $style = !empty($option['image_process']) ? esc_attr($option['image_process']) : '';
     $upload_url_path = esc_attr($option['upload_url_path']);
     if (empty($style)) {
@@ -423,7 +427,7 @@ function uss_custom_image_srcset($sources, $size_array, $image_src, $image_meta,
 
 function uss_setting_content_img_process($content)
 {
-    $option = get_option('uss_options');
+    $option = get_option('uss_options', uss_get_default_options());
     $style = esc_attr($option['image_process']);
     $upload_url_path = esc_attr($option['upload_url_path']);
     if (!empty($style)) {
@@ -445,7 +449,7 @@ function uss_setting_content_img_process($content)
 
 function uss_setting_post_thumbnail_img_process($html, $post_id, $post_image_id)
 {
-    $option = get_option('uss_options');
+    $option = get_option('uss_options', uss_get_default_options());
     $style = esc_attr($option['image_process']);
     $upload_url_path = esc_attr($option['upload_url_path']);
     if (!empty($style) && has_post_thumbnail()) {
@@ -514,23 +518,27 @@ function uss_setting_page()
 
     // 替换数据库链接
     if (!empty($_POST) and $_POST['type'] == 'upyun_uss_replace') {
+        $nonce = $_POST['upyun_uss_replace-nonce'] ?? '';
+        if (empty($nonce) || !wp_verify_nonce($nonce, 'upyun_uss_replace')) {
+            wp_die('Illegal requests!');
+        }
+
         $old_url = esc_url_raw($_POST['old_url']);
         $new_url = esc_url_raw($_POST['new_url']);
 
-        if (empty($oldurl) || empty($newurl)) {
-            echo '<div class="error"><p><strong>替换域名不能为空。</strong></p></div>';
-        } else {
+        if (!empty($old_url) && !empty($new_url)) {
             global $wpdb;
-
             // 文章内容
-            $posts_name = $wpdb->prefix .'posts';
-            $posts_result = $wpdb->query("UPDATE $posts_name SET post_content = REPLACE( post_content, '$old_url', '$new_url') ");
+            $posts_name = $wpdb->prefix . 'posts';
+            $posts_result = $wpdb->query($wpdb->prepare("UPDATE $posts_name SET post_content = REPLACE(post_content, '%s', '%s')", [$old_url, $new_url]));
 
             // 修改题图之类的
-            $postmeta_name = $wpdb->prefix .'postmeta';
-            $postmeta_result = $wpdb->query("UPDATE $postmeta_name SET meta_value = REPLACE( meta_value, '$old_url', '$new_url') ");
+            $postmeta_name = $wpdb->prefix . 'postmeta';
+            $postmeta_result = $wpdb->query($wpdb->prepare("UPDATE $postmeta_name SET meta_value = REPLACE(meta_value, '%s', '%s')", [$old_url, $new_url]));
 
-            echo '<div class="updated"><p><strong>替换成功！共替换文章内链'.$posts_result.'条、题图链接'.$postmeta_result.'条！</strong></p></div>';
+            echo '<div class="updated"><p><strong>替换成功！共替换文章内链' . $posts_result . '条、题图链接' . $postmeta_result . '条！</strong></p></div>';
+        } else {
+            echo '<div class="error"><p><strong>请填写资源链接URL地址！</strong></p></div>';
         }
     }
 
@@ -540,14 +548,14 @@ function uss_setting_page()
         update_option('uss_options', $options);
 
         $upload_path = sanitize_text_field(trim(stripslashes($_POST['upload_path']), '/'));
-        $upload_path = ($upload_path == '') ? ('wp-content/uploads') : ($upload_path);
+        $upload_path = $upload_path == '' ? 'wp-content/uploads' : $upload_path;
         update_option('upload_path', $upload_path);
         $upload_url_path = sanitize_text_field(trim(stripslashes($_POST['upload_url_path']), '/'));
         update_option('upload_url_path', $upload_url_path);
         echo '<div class="updated"><p><strong>设置已保存！</strong></p></div>';
     }
 
-    $uss_options = get_option('uss_options', true);
+    $uss_options = get_option('uss_options', uss_get_default_options());
     $upload_path = get_option('upload_path');
     $upload_url_path = get_option('upload_url_path');
 
@@ -556,16 +564,18 @@ function uss_setting_page()
     $uss_OperatorPwd = esc_attr($uss_options['OperatorPwd']);
 
     $uss_nothumb = esc_attr($uss_options['nothumb']);
-    $uss_nothumb = $uss_nothumb == 'true';
+    $check_nothumb = $uss_nothumb == 'true' ? 'checked="checked"' : '';
 
     $uss_nolocalsaving = esc_attr($uss_options['nolocalsaving']);
-    $uss_nolocalsaving = $uss_nolocalsaving == 'true';
+    $check_nolocalsaving = $uss_nolocalsaving == 'true' ? 'checked="checked"' : '';
 
     $uss_update_file_name = esc_attr($uss_options['update_file_name']);
 
     $uss_image_process = esc_attr($uss_options['image_process']);
 
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? 'https://' : 'http://';
+
+    $nonce = wp_nonce_field('upyun_uss_replace', 'upyun_uss_replace-nonce', true, false);
     ?>
     <div class="wrap" style="margin: 10px;">
         <h1>又拍云 USS <span style="font-size: 13px;">当前版本：<?php echo USS_VERSION; ?></span></h1>
@@ -602,7 +612,7 @@ function uss_setting_page()
                         <legend>不上传缩略图</legend>
                     </th>
                     <td>
-                        <input type="checkbox" name="nothumb" <?php if ($uss_nothumb) { echo 'checked="checked"'; } ?> />
+                        <input type="checkbox" name="nothumb" <?php echo $check_nothumb; ?> />
                         <p>建议不勾选</p>
                     </td>
                 </tr>
@@ -611,7 +621,7 @@ function uss_setting_page()
                         <legend>不在本地保留备份</legend>
                     </th>
                     <td>
-                        <input type="checkbox" name="nolocalsaving" <?php if ($uss_nolocalsaving) { echo 'checked="checked"'; } ?> />
+                        <input type="checkbox" name="nolocalsaving" <?php echo $check_nolocalsaving; ?> />
                         <p>建议不勾选</p>
                     </td>
                 </tr>
@@ -620,11 +630,11 @@ function uss_setting_page()
                         <legend>自动重命名文件</legend>
                     </th>
                     <td>
-                        <select name="update_file_name">
-                            <option <?php if ($uss_update_file_name == 'false') {echo 'selected="selected"';} ?> value="false">不处理</option>
-                            <option <?php if ($uss_update_file_name == 'md5') {echo 'selected="selected"';} ?> value="md5">MD5</option>
-                            <option <?php if ($uss_update_file_name == 'time') {echo 'selected="selected"';} ?> value="time">时间戳+随机数</option>
-                        </select>
+                      <select name="update_file_name">
+                        <option <?php echo $uss_update_file_name == 'false' ? 'selected="selected"' : '';?> value="false">不处理</option>
+                        <option <?php echo $uss_update_file_name == 'md5' ? 'selected="selected"' : '';?> value="md5">MD5</option>
+                        <option <?php echo $uss_update_file_name == 'time' ? 'selected="selected"' : '';?> value="time">时间戳+随机数</option>
+                      </select>
                     </td>
                 </tr>
                 <tr>
@@ -633,7 +643,7 @@ function uss_setting_page()
                     </th>
                     <td>
                         <input type="text" name="upload_path" value="<?php echo $upload_path; ?>" size="50" placeholder="请输入上传文件夹"/>
-                        <p>附件在服务器上的存储位置，例如： <code>wp-content/uploads</code> （注意不要以“/”开头和结尾），根目录请输入<code>.</code>。</p>
+                        <p>附件在服务器上的存储位置，例如：<code>wp-content/uploads</code> （注意不要以“/”开头和结尾），根目录请输入<code>.</code>。</p>
                     </td>
                 </tr>
                 <tr>
@@ -686,7 +696,7 @@ function uss_setting_page()
                     <input type="hidden" name="type" value="upyun_uss_all">
                     <td>
                         <input type="submit" class="button button-secondary" value="开始同步"/>
-                        <p><b>注意：如果是首次同步，执行时间将会十分十分长（根据你的历史附件数量），有可能会因执行时间过长，页面显示超时或者报错。<br> 所以，建议那些几千上万附件的大神们，考虑官方的 <a target="_blank" rel="nofollow" href="https://help.upyun.com/knowledge-base/developer_tools/">同步工具</a></b></p>
+                        <p><b>注意：如果是首次同步，执行时间将会十分十分长（根据你的历史附件数量），有可能会因执行时间过长，页面显示超时或者报错。<br> 所以，建议那些几千上万附件的用户考虑官方的 <a target="_blank" rel="nofollow" href="https://help.upyun.com/knowledge-base/developer_tools/">同步工具</a></b></p>
                     </td>
                 </tr>
             </table>
@@ -717,6 +727,7 @@ function uss_setting_page()
                         <legend></legend>
                     </th>
                     <input type="hidden" name="type" value="upyun_uss_replace">
+                    <?php echo $nonce; ?>
                     <td>
                         <input type="submit" class="button button-secondary" value="开始替换"/>
                         <p><b>注意：如果是首次替换，请注意备份！此功能会替换文章以及设置的特色图片（题图）等使用的资源链接</b></p>
